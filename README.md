143cf30f1b21dd34ea4ce3e75202a4c58ff9f68c
Gajus Kuizinas <gk@anuary.com>
2015-12-07 21:40:21 +0000

    Using mkdirp to create a folder when it does not exist. Using devServer.outputPath instead of output.path configuration.

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 [![NPM version](http://img.shields.io/npm/v/write-file-webpack-plugin.svg?style=flat)](https://www.npmjs.com/package/write-file-webpack-plugin)
 [![js-canonical-style](https://img.shields.io/badge/code%20style-canonical-brightgreen.svg?style=flat)](https://github.com/gajus/canonical)
 
-Forces webpack-dev-server to write bundle files to the file system.
+Forces `webpack-dev-server` program to write bundle files to the file system.
+
+This plugin has no effect when `webpack` program is used.
 
 ## Install
 
@@ -13,13 +15,26 @@ npm install write-file-webpack-plugin --save-dev
 
 ## Usage
 
+Configure [`webpack.config.js`](https://webpack.github.io/docs/configuration.html) to use the `write-file-webpack-plugin` plugin.
+
+You must add `outputPath` property to the `devServer` configuration (For explanation see https://github.com/gajus/write-file-webpack-plugin/issues/1). `devServer.outputPath` value must be equal to `output.path` configuration value.
+
 ```js
+import path from 'path';
 import WriteFilePlugin from 'write-file-webpack-plugin';
 
 export default {
-    // ...
+    devServer: {
+        outputPath: path.join(__dirname, './dist')
+    },
+    output: {
+        path: path.join(__dirname, './dist')
+    },
     plugins: [
         new WriteFilePlugin()
-    ]
+    ],
+    // ...
 }
 ```
+
+See [./sandbox](./sandbox) for a working `webpack` configuration.
--- src/index.js
+++ src/index.js
@@ -1,35 +1,56 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
+import mkdirp from 'mkdirp';
 
-let apply;
+let apply,
+    isMemoryFileSystem;
+
+/**
+ * When 'webpack' program is used, constructor name is equal to 'NodeOutputFileSystem'.
+ * When 'webpack-dev-server' program is used, constructor name is equal to 'MemoryFileSystem'.
+ *
+ * @param {Object} outputFileSystem
+ * @returns {boolean}
+ */
+isMemoryFileSystem = (outputFileSystem) => {
+    return outputFileSystem.__proto__.constructor.name === 'MemoryFileSystem';
+};
 
 apply = (compiler) => {
     compiler.plugin('done', (stats) => {
-        let files;
+        let files,
+            outputPath;
+
+        if (!isMemoryFileSystem(compiler.outputFileSystem)) {
+            return;
+        }
 
         if (stats.compilation.errors.length) {
             return;
         }
 
+        if (!compiler.options.devServer.outputPath) {
+            throw new Error('devServer.outputPath is not defined.');
+        }
+
+        outputPath = compiler.options.devServer.outputPath;
+
+        mkdirp.sync(outputPath);
+
         files = _.map(stats.compilation.chunks, 'files');
+        files = _.flatten(files);
 
-        _.forEach(files, (fileName) => {
-            let filePath;
+        _.forEach(files, (bundleFileName) => {
+            let bundleBody,
+                bundleFilePath,
+                outputFilePath;
 
-            if (!compiler.options.output.path) {
-                throw new Error('output.path is not defined.');
-            }
+            bundleFilePath = path.join('/', bundleFileName);
+            bundleBody = compiler.outputFileSystem.readFileSync(bundleFilePath);
+            outputFilePath = path.join(outputPath, bundleFileName);
 
-            filePath = path.join(compiler.options.output.path, fileName);
-
-            compiler.outputFileSystem.readFile(filePath, 'utf-8', (errorRead, body) => {
-                if (errorRead) {
-                    throw new Error('Cannot read input file.');
-                }
-
-                fs.writeFileSync(filePath, body);
-            });
+            fs.writeFileSync(outputFilePath, bundleBody);
         });
     });
 };
