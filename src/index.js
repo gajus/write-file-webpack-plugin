56d88a0da697dde296d7822964eb0c453d4fd350
Gajus Kuizinas <gk@anuary.com>
2015-12-07 20:23:02 +0000

    Reduce nesting.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
-export default () => {
-    return {
-        apply: (compiler) => {
-            compiler.plugin('done', (stats) => {
-                if (stats.compilation.errors.length) {
-                    return;
+let apply;
+
+apply = (compiler) => {
+    compiler.plugin('done', (stats) => {
+        let files;
+
+        if (stats.compilation.errors.length) {
+            return;
+        }
+
+        files = _.map(stats.compilation.chunks, 'files');
+
+        _.forEach(files, (fileName) => {
+            let filePath;
+
+            if (!compiler.options.output.path) {
+                throw new Error('output.path is not defined.');
+            }
+
+            filePath = path.join(compiler.options.output.path, fileName);
+
+            compiler.outputFileSystem.readFile(filePath, 'utf-8', (errorRead, body) => {
+                if (errorRead) {
+                    throw new Error('Cannot read input file.');
                 }
 
-                stats.compilation.chunks.forEach((chunk) => {
-                    chunk.files.forEach((fileName) => {
-                        let filePath;
-
-                        if (!compiler.options.output.path) {
-                            throw new Error('output.path is not defined.');
-                        }
-
-                        filePath = path.join(compiler.options.output.path, fileName);
-
-                        compiler.outputFileSystem.readFile(filePath, 'utf-8', (errorRead, body) => {
-                            if (errorRead) {
-                                throw new Error('Cannot read input file.');
-                            }
-                            fs.writeFile(filePath, body, (errorWrite) => {
-                                if (errorWrite) {
-                                    throw new Error('Cannot write output file.');
-                                }
-                            });
-                        });
-                    });
-                });
+                fs.writeFileSync(filePath, body);
             });
-        }
+        });
+    });
+};
+
+export default () => {
+    return {
+        apply
     };
 };
