9fdf789922e3e55f3da266a7ebe156c480f10a77
Gajus Kuizinas <gk@anuary.com>
2015-12-07 21:39:30 +0000

    Added setup example.

---
+++ sandbox/src/app/index.js
@@ -0,0 +1,3 @@
+require('./foo');
+
+console.log('test');
---
+++ sandbox/webpack.config.js
@@ -0,0 +1,39 @@
+var devServer,
+    webpack,
+    path,
+    WriteFileWebpackPlugin;
+
+webpack = require('webpack');
+path = require('path');
+WriteFileWebpackPlugin = require('./../dist').default;
+
+devServer = {
+    outputPath: path.join(__dirname, './dist'),
+    contentBase: path.resolve(__dirname, './src'),
+    colors: true,
+    quiet: false,
+    noInfo: false,
+    publicPath: '/static/',
+    historyApiFallback: true,
+    host: '127.0.0.1',
+    port: 8000,
+    hot: false
+};
+
+module.exports = {
+    devtool: 'source-map',
+    debug: false,
+    devServer: devServer,
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './app'
+    },
+    output: {
+        path: devServer.outputPath,
+        filename: '[name].js',
+        publicPath: devServer.publicPath
+    },
+    plugins: [
+        new WriteFileWebpackPlugin()
+    ]
+};
